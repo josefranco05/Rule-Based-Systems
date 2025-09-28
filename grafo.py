a6c615d294fa45c37052c850173ab9ee9d2093ec
Bareval6 <bareval6@estudiante.ibero.edu.co>
2025-09-27 21:59:00 -0500

    Comentarios de codigo

--- grafo.py
+++ grafo.py
@@ -1,14 +1,16 @@
+# Importamos librerías necesarias
 import contextily as ctx
 import matplotlib.pyplot as plt
 import networkx as nx
 
+# Definimos la función que grafica el grafo sobre un mapa
 def graficar(G, gdf):
     gdf_web = gdf.to_crs(epsg=3857)
     pos = {
         row.objectid: (geom.x, geom.y)
         for row, geom in zip(gdf_web.itertuples(), gdf_web.geometry)
     }
-
+# Creamos la figura y el eje de dibujo con un tamaño grande
     fig, ax = plt.subplots(figsize=(20, 20))
     gdf_web.plot(ax=ax, alpha=0)  # sólo para fijar extents
     
@@ -21,7 +23,7 @@ def graficar(G, gdf):
         edge_color="gray",
         node_size=40,
     )
-
+# Añadimos un mapa base (fondo) estilo claro de CartoDB
     ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
     ax.set_axis_off()
     plt.show()
